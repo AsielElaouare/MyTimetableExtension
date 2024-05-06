cc7d46a6d9326f524021ed1bb04cf9734bd57aa4
iolite312 <leonwatertor18@gmail.com>
2024-05-06 12:41:43 +0200

    Revamped to only needing 1 post request

--- js/script.js
+++ js/script.js
@@ -8,122 +8,104 @@ const spinner = document.getElementById("spinner");
 const schedule = document.getElementById("schedule");
 const submitBtn = document.getElementById("submitBtn")
 
-
 submitBtn.addEventListener('click', sendURL);
 
 previousWeekBtnTag.addEventListener('click', previousWeekBtn);
 nextWeekBtnTag.addEventListener('click', nextWeekBtn);
 
-
 const currentWeekNow = currentWeek();
 
 let currentDisplayedWeek = null;
 
-async function sendURL(){
+async function sendURL() {
     let isValidURL = validatorURL(input.value);
-    if(input.value != "" && isValidURL != false){
-        try{
-        const res = await fetch(baseUrl, {
-            method: 'POST',
-            headers: {
-                "Content-Type": 'application/json'
-            },
-            body: JSON.stringify ({
-                URL: input.value
-            })
-        });
-        localStorage.setItem("icalURL", input.value);
-        getData(currentWeekNow);
+    if (input.value != "" && isValidURL != false) {
+        try {
+            const res = await fetch(baseUrl, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": 'application/json'
+                },
+                body: JSON.stringify({
+                    URL: input.value
+                })
+            });
+            localStorage.setItem("icalURL", input.value);
+            const data = await res.json();
+            localStorage.setItem("data", JSON.stringify(data));
+            getData(currentWeekNow);
         }
-        catch{
+        catch {
             console.log("Server is Down")
         }
     };
 };
 
-
 function validatorURL(url) {
     try {
-      new URL(url);
-      return true;
+        new URL(url);
+        return true;
     } catch (error) {
-      return false;
+        return false;
     }
-  }
-  
+}
 
-function showInputURL(){
+function showInputURL() {
     input.value = localStorage.getItem("icalURL")
 }
 
-async function icalRequest(){
-    sendURL();
-    try
-    {
-        const res = await fetch(baseUrl, {
-            method: "GET"
-        });
-        const data = await res.json();
-        localStorage.setItem("data", JSON.stringify(data));
-    }
-    catch (error){
-        console.error("Error fetchnig data:", error);
-    };
-}
-
-async function getData(weekDate){
+async function getData(weekDate) {
     loadSpinner();
     const localData = JSON.parse(localStorage.getItem("data"));
-    loadLessons(localData, weekDate);   
+    loadLessons(localData, weekDate);
 };
 
-function loadLessons(data, currentWeekVar){
+function loadLessons(data, currentWeekVar) {
     deleteExistentData();
     data.forEach(lesson => {
         const { startDate, endDate, lastDay } = getLessonDays(lesson, currentWeekVar)
         lastDay.setDate(currentWeekVar.getDate() + LAST_DAY_OF_WEEK);
-        if(startDate >= currentWeekVar && startDate <= lastDay)
-        {
+        if (startDate >= currentWeekVar && startDate <= lastDay) {
             displayLessons(lesson, startDate, endDate, currentWeekVar);
             displayWeekHeader(currentWeekVar);
 
-            currentDisplayedWeek =  currentWeekVar; 
+            currentDisplayedWeek = currentWeekVar;
 
         }
-        else{
+        else {
             currentDisplayedWeek = currentWeekNow;
         }
-    });  
+    });
     displayWeekHeader(currentWeekVar);
-    laodSchedule();
+    loadSchedule();
 };
 
-function nextWeek(){
+function nextWeek() {
     let currentWeekVar = currentDisplayedWeek;
-    currentWeekVar.setDate(currentWeekVar.getDate() + 7); 
+    currentWeekVar.setDate(currentWeekVar.getDate() + 7);
     console.log("inside next function:", currentWeekVar);
     console.log(currentDisplayedWeek);
     return currentWeekVar;
 };
 
-function nextWeekBtn(){
+function nextWeekBtn() {
     let week = nextWeek();
     getData(week);
 };
 
-function previousWeek(){
+function previousWeek() {
     let currentWeekVar = currentDisplayedWeek;
-    currentWeekVar.setDate(currentWeekVar.getDate() - 7); 
+    currentWeekVar.setDate(currentWeekVar.getDate() - 7);
     console.log("inside previous function:", currentWeekVar);
     return currentWeekVar;
 };
 
-function previousWeekBtn(){
+function previousWeekBtn() {
     let week = previousWeek();
     getData(week);
 };
 
-function getLessonDays(lesson, currentWeekVar){
+function getLessonDays(lesson, currentWeekVar) {
     const startDateString = lesson["DTSTART;TZID=Europe/Brussels"].replace(/\r/g, '');
     const endDateString = lesson["DTEND;TZID=Europe/Brussels"].replace(/\r/g, '');
     const isoStartDate = isoFormat(startDateString);
@@ -135,15 +117,14 @@ function getLessonDays(lesson, currentWeekVar){
     return { startDate, endDate, lastDay };
 };
 
-function currentWeek(){
+function currentWeek() {
     const today = new Date();
     const startOfWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - today.getDay() + 1);
     startOfWeek.setDate(startOfWeek.getDate());
     return startOfWeek;
 };
 
-
-function displayLessons(lesson, startDate, endDate, currentWeekVar){
+function displayLessons(lesson, startDate, endDate, currentWeekVar) {
     const div = document.createElement("div");
     const pTime = document.createElement("p");
     const pLocation = document.createElement("p");
@@ -159,28 +140,27 @@ function displayLessons(lesson, startDate, endDate, currentWeekVar){
     pTime.innerText = startHour + "-" + endHour;
     pLocation.innerText = lesson.LOCATION;
     pSummary.innerText = lesson.SUMMARY;
-    
+
     div.appendChild(pTime);
     div.appendChild(pLocation);
     div.appendChild(pSummary);
     appendWeekDayHtmlTable(div, startDate);
 };
 
-
-function deleteExistentData(){
+function deleteExistentData() {
     const lessonCells = document.querySelectorAll('[id^="lessons-"]');
     lessonCells.forEach(cell => {
         cell.innerHTML = '';
     });
 };
 
-function displayWeekHeader(currentWeekVar){
+function displayWeekHeader(currentWeekVar) {
     let monthHeader = document.getElementById("month");
     const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri"];
     const week = new Date(currentWeekVar);
-    const day = new Date(currentWeekVar); 
+    const day = new Date(currentWeekVar);
     let dayNumber = week.getDate();
-    for(let i=  0; i <5; i++){
+    for (let i = 0; i < 5; i++) {
         let weekHtmlHeader = document.getElementById(daysOfWeek[i]);
         weekHtmlHeader.innerText = daysOfWeek[i] + " " + dayNumber;
         day.setDate(day.getDate() + 1);
@@ -191,30 +171,29 @@ function displayWeekHeader(currentWeekVar){
     monthHeader.innerText = month + ", week " + weekNr;
 };
 
-function appendWeekDayHtmlTable(div, startDate){
+function appendWeekDayHtmlTable(div, startDate) {
     const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-    const dayOfWeek = (startDate.getDay() + 6) % 7; 
+    const dayOfWeek = (startDate.getDay() + 6) % 7;
     const dayOfWeekString = daysOfWeek[dayOfWeek];
 
     let weekDayHtml = document.getElementById("lessons-" + dayOfWeekString);
 
-    switch (dayOfWeekString) 
-    {
+    switch (dayOfWeekString) {
         case 'Monday':
             weekDayHtml.appendChild(div);
-          break;
+            break;
         case 'Tuesday':
             weekDayHtml.appendChild(div);
-           break;
+            break;
         case 'Wednesday':
             weekDayHtml.appendChild(div);
-          break;
-          case 'Thursday':
+            break;
+        case 'Thursday':
             weekDayHtml.appendChild(div);
-          break;
-          case 'Friday':
+            break;
+        case 'Friday':
             weekDayHtml.appendChild(div);
-          break;
+            break;
         default:
     };
 };
@@ -238,24 +217,24 @@ function getWeekNumber(date) {
     date = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
     // Set to nearest Thursday: current date + 4 - current day number
     // Make Sunday's day number 7
-    date.setUTCDate(date.getUTCDate() + 4 - (date.getUTCDay()||7));
+    date.setUTCDate(date.getUTCDate() + 4 - (date.getUTCDay() || 7));
     // Get first day of year
-    var yearStart = new Date(Date.UTC(date.getUTCFullYear(),0,1));
+    var yearStart = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
     // Calculate full weeks to nearest Thursday
-    var weekNo = Math.ceil(( ( (date - yearStart) / 86400000) + 1)/7);
+    var weekNo = Math.ceil((((date - yearStart) / 86400000) + 1) / 7);
     // Return array of year and week number
     return weekNo;
 }
 
-function loadSpinner(){
+function loadSpinner() {
     spinner.style.visibility = "visible";
     schedule.style.visibility = "hidden";
 };
 
-function laodSchedule(){
+function loadSchedule() {
     spinner.style.visibility = "hidden";
     schedule.style.visibility = "visible";
 };
 showInputURL();
-icalRequest();
+sendURL();
 getData(currentWeekNow);
